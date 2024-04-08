b3c0e171a1888d5e1bfb9d177548a9c467fadb56
csvenke <csvenke@users.noreply.github.com>
2024-04-09 00:41:23 +0200

    feat: improve dev script

--- scripts/dev/script.bash
+++ scripts/dev/script.bash
@@ -1,27 +1,34 @@
-function findProjectRootDirectories() {
-	local search_path="$1"
-	local root_files="(.git|shell.nix|flake.nix|.envrc|.env)"
-	ag --hidden -g "$root_files" "$search_path" | xargs -I {} dirname {} | sort -u
+function findRoots() {
+	local search_dir=$1
+	local root_files="(.git|shell.nix|flake.nix|.envrc|.env|.sln|.csproj|pom.xml|settings.gradle|settings.gradke.kts|package.json)"
+	ag --hidden -g "$root_files" "$search_dir" |
+		xargs -I {} dirname {} |
+		sort -u |
+		sed "s|^$search_dir/||" |
+		awk -v prefix="$search_dir" 'BEGIN { gray="\033[90m"; blue="\033[34m"; reset="\033[0m"; folderIcon=" "; } { print blue folderIcon $0 reset " " gray "(" prefix "/" $0 ")" reset }'
 }
 
-function formatDirectoriesForDisplay() {
-	local search_path="$1"
-	local directories="$2"
-	echo "$directories" |
-		sed "s|^$search_path/||" |
-		awk -v prefix="$search_path" 'BEGIN {
-        gray="\033[90m";
-        reset="\033[0m";
-    }
-    {print $0 " " gray "(" prefix "/" $0 ")" reset}'
+function gatherRoots() {
+	local directory_paths=""
+
+	for dir in "$@"; do
+		if [ -e "$dir" ]; then
+			matches=$(findRoots "$dir")
+			directory_paths=$(printf "%s\n%s" "$directory_paths" "$matches")
+		fi
+	done
+
+	echo "$directory_paths"
 }
 
-function selectDirectoryWithUI() {
+function selectDir() {
 	local formatted_directories="$1"
-	echo "$formatted_directories" | fzf --ansi --border=none | sed 's/.*(\(.*\)).*/\1/'
+	echo "$formatted_directories" |
+		fzf --ansi --border=none |
+		sed 's/.*(\(.*\)).*/\1/'
 }
 
-function navigateAndOpenNvim() {
+function openWithNeovim() {
 	local target_path="$1"
 	if [ -n "$target_path" ]; then
 		cd "$target_path" || exit
@@ -29,12 +36,14 @@ function navigateAndOpenNvim() {
 	fi
 }
 
-function runProjectDirectoryNavigator() {
-	local dir_to_search="$1"
-	root_dirs=$(findProjectRootDirectories "$dir_to_search")
-	formatted_dirs=$(formatDirectoriesForDisplay "$dir_to_search" "$root_dirs")
-	selected_path=$(selectDirectoryWithUI "$formatted_dirs")
-	navigateAndOpenNvim "$selected_path"
+function dev() {
+	if [ $# -eq 0 ]; then
+		return 1
+	fi
+
+	project_dirs=$(gatherRoots "$@")
+	selected_project_dir=$(selectDir "$project_dirs")
+	openWithNeovim "$selected_project_dir"
 }
 
-runProjectDirectoryNavigator "$HOME/projects"
+dev "$HOME/projects" "$HOME/repos"
