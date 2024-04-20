2880933e96153febbc1e14ba16d9c391be20a21a
csvenke <csvenke@users.noreply.github.com>
2024-04-20 19:19:59 +0200

    feat(neovim): use different chatgpt plugin

--- nixpkgs/neovim/config/plugins/chatgpt.lua
+++ nixpkgs/neovim/config/plugins/chatgpt.lua
@@ -1,4 +1,17 @@
-require("chatgpt").setup({})
+require("gp").setup({})
 
-vim.keymap.set("n", "<leader>ap", "<Cmd>ChatGPT<CR>", { desc = "[a]i [p]rompt" })
-vim.keymap.set("n", "<leader>ae", "<Cmd>ChatGPTEditWithInstruction<CR>", { desc = "[a]i [e]dit" })
+vim.keymap.set("n", "<leader>aa", "<cmd>GpNew<cr>", { desc = "quick prompt" })
+vim.keymap.set("n", "<leader>aA", "<cmd>GpChatToggle<cr>", { desc = "toggle chat" })
+
+vim.keymap.set("n", "<leader>a|", "<cmd>GpChatNew vsplit<cr>", { desc = "new chat (vertical)" })
+vim.keymap.set("n", "<leader>a-", "<cmd>GpChatNew split<cr>", { desc = "new chat (horizontal)" })
+vim.keymap.set("n", "<leader>at", "<cmd>GpChatNew tabnew<cr>", { desc = "new chat (tab)" })
+
+vim.keymap.set("v", "<leader>ap", ":'<,'>GpChatPaste split<cr>", { desc = "paste to chat (horizontal)" })
+vim.keymap.set("v", "<leader>aP", ":'<,'>GpChatPaste vsplit<cr>", { desc = "paste to chat (vertical)" })
+
+vim.keymap.set("v", "<leader>ae", ":'<,'>GpRewrite<cr>", { desc = "edit selection" })
+vim.keymap.set("v", "<leader>ar", ":'<,'>GpRewrite<cr>", { desc = "rewrite selection" })
+vim.keymap.set("n", "<leader>ao", "<cmd>GpAppend<cr>", { desc = "insert below" })
+vim.keymap.set("n", "<leader>aO", "<cmd>GpPrepend<cr>", { desc = "insert above" })
+
