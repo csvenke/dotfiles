8b14714904b9a8ec0162dd3bbf4586cf56cc0837
csvenke <csvenke@users.noreply.github.com>
2024-04-21 17:59:06 +0200

    feat(neovim): close specified buffers on q

--- nixpkgs/neovim/config/core/autocmds.lua
+++ nixpkgs/neovim/config/core/autocmds.lua
@@ -14,3 +14,20 @@ vim.api.nvim_create_autocmd("VimResized", {
     vim.cmd("tabnext " .. current_tab)
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+  pattern = {
+    "help",
+    "lspinfo",
+    "notify",
+    "qf",
+    "query",
+    "startuptime",
+    "checkhealth",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
