91ece10ba79784bae9453d9eaf5cbbd2d06f3057
csvenke <csvenke@users.noreply.github.com>
2024-04-28 15:45:14 +0200

    feat!: rename dev environments

--- devenv/dotnet/flake.nix
+++ devenv/dotnet/flake.nix
@@ -1,20 +1,29 @@
 {
-  description = "Dotnet development environment";
+  description = "Combined dotnet development environment";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs.dotnetCorePackages) combinePackages sdk_6_0 sdk_7_0 sdk_8_0 sdk_9_0;
+        dotnet = (combinePackages [
+          sdk_6_0
+          sdk_7_0
+          sdk_8_0
+        ]);
       in
       {
         devShell = pkgs.mkShell {
           packages = [
-            pkgs.dotnet-sdk
+            dotnet
           ];
+          shellHook = ''
+            export DOTNET_ROOT="${dotnet}"
+          '';
         };
       }
     );
