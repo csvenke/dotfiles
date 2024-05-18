ea4f771bad3a627d5a7b3ae48c9dccb3070229db
csvenke <csvenke@users.noreply.github.com>
2024-05-18 16:15:20 +0200

    feat(neovim): update chatgpt keymaps

--- nixpkgs/neovim/config/plugins/chatgpt.lua
+++ nixpkgs/neovim/config/plugins/chatgpt.lua
@@ -28,19 +28,26 @@ require("gp").setup({
   },
 })
 
-vim.keymap.set("n", "<leader>aa", "<cmd>GpNew<cr>", { desc = "quick prompt" })
+vim.keymap.set("n", "<leader>aa", "<cmd>GpChatNew<cr>", { desc = "new chat" })
+vim.keymap.set("n", "<leader>ac", "<cmd>GpChatNew<cr>", { desc = "new chat" })
+
 vim.keymap.set("n", "<leader>aA", "<cmd>GpChatToggle<cr>", { desc = "toggle chat" })
 
 vim.keymap.set("n", "<leader>a|", "<cmd>GpChatNew vsplit<cr>", { desc = "new chat (vertical)" })
 vim.keymap.set("n", "<leader>av", "<cmd>GpChatNew vsplit<cr>", { desc = "new chat (vertical)" })
+
 vim.keymap.set("n", "<leader>a-", "<cmd>GpChatNew split<cr>", { desc = "new chat (horizontal)" })
 vim.keymap.set("n", "<leader>as", "<cmd>GpChatNew split<cr>", { desc = "new chat (horizontal)" })
-vim.keymap.set("n", "<leader>at", "<cmd>GpChatNew tabnew<cr>", { desc = "new chat (tab)" })
 
-vim.keymap.set("v", "<leader>ap", ":'<,'>GpChatPaste split<cr>", { desc = "paste to chat (horizontal)" })
-vim.keymap.set("v", "<leader>aP", ":'<,'>GpChatPaste vsplit<cr>", { desc = "paste to chat (vertical)" })
+vim.keymap.set("v", "ap", ":'<,'>GpChatPaste<cr>", { desc = "paste selection to chat" })
+vim.keymap.set("n", "<leader>ap", ":%GpChatPaste<cr>", { desc = "paste buffer to chat" })
 
-vim.keymap.set("v", "<leader>ae", ":'<,'>GpRewrite<cr>", { desc = "edit selection" })
-vim.keymap.set("v", "<leader>ar", ":'<,'>GpRewrite<cr>", { desc = "rewrite selection" })
+vim.keymap.set("v", "ar", ":'<,'>GpRewrite<cr>", { desc = "rewrite selection" })
+vim.keymap.set("n", "<leader>ar", ":%GpRewrite<cr>", { desc = "rewrite buffer" })
+
+vim.keymap.set("v", "ao", ":'<,'>GpAppend<cr>", { desc = "insert below selection" })
+vim.keymap.set("v", "aO", ":'<,'>GpPrepend<cr>", { desc = "insert above selection" })
 vim.keymap.set("n", "<leader>ao", "<cmd>GpAppend<cr>", { desc = "insert below" })
 vim.keymap.set("n", "<leader>aO", "<cmd>GpPrepend<cr>", { desc = "insert above" })
+
+vim.keymap.set("n", "<leader>af", "<cmd>GpChatFinder<cr>", { desc = "find chats" })
