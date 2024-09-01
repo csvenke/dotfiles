badd46e13479f1ea13ae3642eb21a0782f113e67
csvenke <csvenke@users.noreply.github.com>
2024-09-01 14:07:50 +0200

    feat(neovim): improve vimrc by loading each plugin separately

--- packages/neovim/config/default.nix
+++ packages/neovim/config/default.nix
@@ -1,30 +1,31 @@
 { pkgs }:
 
-with builtins;
-
 let
-  inherit (pkgs.lib) pipe strings;
+  inherit (builtins) readDir readFile toFile attrNames concatStringsSep filter;
+  inherit (pkgs.lib) pipe strings lists;
 
-  readLuaDir = dir:
-    pipe dir [
+  mkFile = name: path:
+    (toFile name (readFile path));
+
+  mkFiles = suffix: directory:
+    pipe directory [
       readDir
       attrNames
-      (filter (name: strings.hasSuffix ".lua" name))
-      (map (name: readFile "${dir}/${name}"))
-      (concatStringsSep "\n")
+      (filter (name: strings.hasSuffix suffix name))
+      (map (name: mkFile name "${directory}/${name}"))
     ];
-
 
   mkVimRc = files:
     pipe files [
+      lists.flatten
       (map (file: "luafile ${file}"))
       (concatStringsSep "\n")
     ];
 in
 
 mkVimRc [
-  (toFile "options.lua" (readFile ./core/options.lua))
-  (toFile "keymaps.lua" (readFile ./core/keymaps.lua))
-  (toFile "autocmds.lua" (readFile ./core/autocmds.lua))
-  (toFile "plugins.lua" (readLuaDir ./plugins))
+  (mkFile "options.lua" ./core/options.lua)
+  (mkFile "keymaps.lua" ./core/keymaps.lua)
+  (mkFile "autocmds.lua" ./core/autocmds.lua)
+  (mkFiles ".lua" ./plugins)
 ]
