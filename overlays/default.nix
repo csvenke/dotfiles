b0061e53d0d21066be200c0c985f2876284d4869
csvenke <csvenke@users.noreply.github.com>
2026-02-22 22:28:55 +0100

    chore: remove unused context7-mcp package

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,4 @@
 final: prev: {
-  context7-mcp = prev.callPackage ./context7-mcp/package.nix { };
   fzf = prev.callPackage ./fzf/package.nix { fzf = prev.fzf; };
   tmux = prev.callPackage ./tmux/package.nix { tmux = prev.tmux; };
 }
